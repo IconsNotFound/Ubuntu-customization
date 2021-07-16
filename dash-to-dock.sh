a540e4687076a2ab97dddd06cf8a7fd7e5825053
IconsNotFound <76126001+IconsNotFound@users.noreply.github.com>
2021-07-16 13:40:29 +0600

    Create dash-to-dock.sh

---
+++ dash-to-dock.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews'
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+gsettings set org.gnome.shell.extensions.dash-to-dock intellihide true
+gsettings set org.gnome.shell.extensions.dash-to-dock force-straight-corner false
+gsettings set org.gnome.shell.extensions.dash-to-dock apply-custom-theme true
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts true
+gsettings set org.gnome.shell.extensions.dash-to-dock show-trash true
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
