aeffecb091bbd9afc92d0550b701f609b9162c82
IconsNotFound <76126001+IconsNotFound@users.noreply.github.com>
2021-07-16 18:36:27 +0600

    Create ding.sh

---
+++ ding.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+gsettings set org.gnome.shell.extensions.ding show-trash false
+gsettings set org.gnome.shell.extensions.ding show-home true
+gsettings set org.gnome.shell.extensions.ding show-volumes false
